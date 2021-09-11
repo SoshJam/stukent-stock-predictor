3defb063be21259f8f1a01714516881029cda8bb
Sam Hill <soshjam@soshjam.com>
2021-09-10 23:02:30 -0600

    Added Hank Auto Co

--- src/App.js
+++ src/App.js
@@ -63,8 +63,8 @@ function App() {
             />
             
             <StockTableRow
-              company="Swoosh Athletics (SA)"
-              irl="Nike Inc. (NKE)*"
+              company="Swoosh Athletics (SA)*"
+              irl="Nike Inc. (NKE)"
               name="SA_NKE"
             />
             
@@ -79,6 +79,12 @@ function App() {
               irl="American Airlines Group Inc. (AAL)"
               name="SUF_AAL"
             />
+            
+            <StockTableRow
+              company="Hank Auto Co. (HAC)"
+              irl="Ford Motor Company (F)"
+              name="HAC_F"
+            />
           </tbody>
         </table>
         <p class="tiny"><em>* For some reason, this stock is always 2-3 cents off from the IRL price, so be warned. If there's ever a larger discrepancy please let me know.</em></p>
@@ -91,7 +97,7 @@ function App() {
 
         <p>The listed share prices are the ones the market <strong>closes</strong> on on a given day. Stukent shows the closing prices from the day before. If you look at the graph there, it'll say {getDate(-1)} instead of {getDate(0)}. If it's confusing, you can think of it like "This is how much a share will be worth by midnight that night." You want to invest in the ones that will be worth the most soonest.</p>
 
-        <p>Additionally, you may notice that some stocks are missing, namely MFCR, RWCIX, MIH, and SP. I wasn't able to figure out what they meant, so if you can figure out please let me know and I'll add it to the site.</p>
+        <p>Additionally, you may notice that some stocks are missing, namely MFCR, RWCIX, MIH, and SP. I wasn't able to figure out what they meant, so if you can figure out please let me know and I'll add it to the site. Also I'm 99% Run Fast Telecom (RFT) is Sprint Mobile, but they merged with T-Mobile in 2020 and I can't find their history anywhere.</p>
 
         <p>See the <a href="https://github.com/SoshJam/stukent-stock-predictor#readme" target="_blank" rel="noreferrer">Github README</a> for more information.</p>
 
