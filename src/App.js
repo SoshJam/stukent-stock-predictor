36ca7efcee2db01d0627e0c191377b19c43514dc
Sam Hill <soshjam@soshjam.com>
2021-09-12 10:51:15 -0600

    Updated "More Info" section.

--- src/App.js
+++ src/App.js
@@ -115,26 +115,26 @@ function App() {
             />
           </tbody>
         </table>
-        <p class="tiny"><em>* For some reason, these stocks are always 2-3 cents off from the IRL price, so be warned. If there's ever a larger discrepancy please let me know.</em></p>
+        <p class="tiny"><em>* For some reason, these stocks are always 2-3 cents off from the IRL <span onClick={() => setUnlocked(!unlocked)}>price,</span> so be warned. If there's ever a larger discrepancy please let me know.</em></p>
 
         <hr/>
 
         <h1>More Info</h1>
 
-        <p>Welcome to Sam Hill's Stukent Stock Predictor! This is still a pretty rudimentary site because I made it in like 5 hours, but eventually it's going to be a lot better. Eventually I will be adding a line chart as well to more easily visualize the stocks.</p>
+        <p>Welcome to Sam Hill's Stukent Stock Predictor!</p>
 
-        <p>The listed share prices are the ones the market <strong>closes</strong> on on a given day. Stukent shows the closing prices from the day before. If you look at the graph there, it'll say {getDate(-1)} instead of {getDate(0)}. If it's confusing, you can think of it like "This is how much a share will be worth by midnight that night." You want to invest in the ones that will be worth the most soonest.</p>
+        <p>The listed share prices are the ones the market <strong>closes</strong> on on a given day. Stukent shows the closing prices from the day before. (If you look at the graph on Stukent, it'll say {getDate(-1)} instead of {getDate(0)}.) If it's confusing, you can think of it like "This is how much a share will be worth by midnight that night."</p>
 
-        <p>Additionally, you may notice that some stocks are missing, namely MFCR, RWCIX, and MIH. I wasn't able to figure out what they meant, so if you can figure out please let me know and I'll add it to the site.</p>
-        
-        <p>Also, I've applied my incredible deduction skills to reach the conclusion that Run Fast Telecom (RFT) is indeed Sprint (shocker), but they merged with T-Mobile last year and so I can't find the correct data. I need everything from 9/1/08 to 9/1/18. One website has slightly inaccurate data and only goes back to Jul 2009. Another has correct data, but only allows you to look it up for one day at a time. If you can find anywhere with accurate data across the needed time frame that allows you to download it in bulk, please reach out to me.</p>
+        <p>Additionally, you may notice that some stocks are missing, namely MFCR, RWCIX, and MIH. I wasn't able to figure out what they meant, so if you can figure out please let me know and I'll add it to the site. Also, I've applied my incredible deduction skills to reach the conclusion that Run Fast Telecom (RFT) is indeed Sprint (shocker), but they merged with T-Mobile last year and so I can't find the correct data. I need everything from 9/1/08 to 9/1/18. One website has slightly inaccurate data and only goes back to Jul 2009. Another has correct data, but only allows you to look it up for one day at a time. If you can find anywhere with accurate data across the needed time frame that allows you to download it in bulk, please reach out to me.</p>
 
-        <p>See the <a href="https://github.com/SoshJam/stukent-stock-predictor#readme" target="_blank" rel="noreferrer">Github README</a> for more information.</p>
+        <p>If you find any bugs, issues, or incorrect prices with a difference larger than 10 cents, please contact me to let me know. Additionally, if you have any suggestions for things you want added to the site, tell me and I'll consider putting it in. My email address is <a href="mailto:soshjam@soshjam.com">soshjam@soshjam.com</a></p>
+
+        <p>See the <a href="https://github.com/SoshJam/stukent-stock-predictor" target="_blank" rel="noreferrer">Github repository</a> for more information.</p>
 
       </main>
 
       <footer>
-        Copyright <span onClick={() => setUnlocked(!unlocked)}>&copy;</span> {new Date().getFullYear()} Sam Hill
+        Copyright &copy; {new Date().getFullYear()} Sam Hill
       </footer>
 
     </div>
